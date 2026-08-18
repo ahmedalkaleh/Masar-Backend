247ac31ab485bce0e08c56066b4762597692fecf
Ahmed <ahmodikaleeh2005@gmail.com>
2026-08-18 18:10:51 +0300

    Feat(Applicaion) add CreatePersonCommandValidator

---
+++ src/Masar.Application/Features/Person/Commands/CreatePerson/CreatePersonCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Person.Commands.CreatePerson
+{
+    internal class CreatePersonCommandValidator
+    {
+    }
+}
