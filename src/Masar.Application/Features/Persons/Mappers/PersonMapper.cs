b2bee4b5d7b6127cf879b8f06519a2cc9f7c1690
Ahmed <ahmodikaleeh2005@gmail.com>
2026-08-18 18:59:10 +0300

    Feat(Application) add PersonMapper

---
+++ src/Masar.Application/Features/Persons/Mappers/PersonMapper.cs
@@ -0,0 +1,30 @@
+﻿using Masar.Application.Features.Persons.Dtos;
+using Masar.Domain.Persons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Masar.Application.Features.Persons.Mappers
+{
+   internal static class PersonMapper
+    {
+        internal static PersonDto ToDto(this Masar.Domain.Persons.Person person)
+        {
+            if (person == null) throw new ArgumentNullException(nameof(person));
+            return new PersonDto
+            {
+                PersonID = person.Id,
+                FullName = person.FullName,
+                Email = person.Email,
+                PhoneNumber = person.PhoneNumber
+            };
+        }
+
+        internal static List<PersonDto> ToDtos(this IEnumerable<Person> entities)
+        {
+            if (entities == null) throw new ArgumentNullException(nameof(entities));
+            return entities.Select(e => e.ToDto()).ToList();
+        }
+    }
+}
