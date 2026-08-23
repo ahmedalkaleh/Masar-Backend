cc1a831de56e7c8eae9638726308ea5c87546206
Ahmed <ahmodikaleeh2005@gmail.com>
2026-08-23 14:21:56 +0300

    feat(app) add UpdatePerson command and validator

---
+++ src/Masar.Application/Features/Persons/Commands/UpdatePerson/UpdatePersonCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Persons.Commands.UpdatePerson
+{
+    internal class UpdatePersonCommandHandler
+    {
+    }
+}
---
+++ src/Masar.Application/Features/Persons/Commands/UpdatePerson/UpdatePersonCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+namespace Masar.Application.Features.Persons.Commands.UpdatePerson
+{
+   public sealed class UpdatePersonCommandValidator : AbstractValidator<UpdatePersonCommand>
+    {
+        public UpdatePersonCommandValidator() {
+            RuleFor(x => x.PersonID)
+               .NotEmpty().WithMessage("Person ID is required.");
+
+            RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("Full name is required.")
+            .MaximumLength(100).WithMessage("Full name must not exceed 100 characters.");
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required.").Matches(@"^\+?[1-9]\d{1,14}$").Matches(@"^\+?\d{7,15}$").WithMessage("Phone number must be 7–15 digits and may start with '+'.");
+
+        }
+    }
+}
