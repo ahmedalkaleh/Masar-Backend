d61b5c59ae63b20251333664b3a2dc1127b3043e
Ahmed <ahmodikaleeh2005@gmail.com>
2026-08-23 15:12:27 +0300

    feat(api) add update person controller

--- src/Masar.API/Controllers/PersonsController.cs
+++ src/Masar.API/Controllers/PersonsController.cs
@@ -2,7 +2,7 @@
 using Masar.Application.Features.Persons.Commands.CreatePerson;
 using Masar.Application.Features.Persons.Dtos;
 using MediatR;
-
+using Masar.Application.Features.Persons.Commands.UpdatePerson;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,5 +30,26 @@ namespace Masar.API.Controllers
             var result = await sender.Send(request, cancellationToken);
             return result.Match(response=>CreatedAtRoute("GetPersonById", new { id = response.PersonID }, response), Problem);
         }
+
+        [HttpPut("{id:guid}")]
+        [ProducesResponseType(typeof(PersonDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [EndpointSummary("Updates an existing person.")]
+        [EndpointDescription("Updates the details of an existing person in the system.")]
+        [EndpointName("UpdatePerson")]
+        public async Task<IActionResult> UpdatePerson( Guid id, [FromBody] UpdatePersonCommand request, CancellationToken cancellationToken)
+        {
+          
+            var command = new UpdatePersonCommand(
+    id,
+    request.FullName,
+    request.Email,
+    request.PhoneNumber
+);
+            var result = await sender.Send(command, cancellationToken);
+            return result.Match(response => Ok(response), Problem);
+        }
+
     }
 }
