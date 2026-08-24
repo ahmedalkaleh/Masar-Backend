c2bb4e9b462d24ceff70b13bcb907c036a217504
Ahmed <ahmodikaleeh2005@gmail.com>
2026-08-24 20:43:04 +0300

    feat(domain)add user and userError classes

---
+++ src/Masar.Domain/Users/UserError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Masar.Domain.Common.Results;
+namespace Masar.Domain.Users
+{
+    public static class UserError
+    {
+        public static readonly Error UsernameRequired =
+            Error.Validation("User_Username_Required", "Username is required.");
+      
+        public static readonly Error PersonIdRequired = Error.Validation("User_PersonId_Required", "Person ID is required.");
+        public static readonly Error UserNotFound = Error.NotFound("User_Not_Found", "User not found.");
+        
+    }
+}
