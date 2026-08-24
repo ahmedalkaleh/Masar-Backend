531461fe8ecdfad0c82882e6b39010d215d16ed9
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-08-24 15:53:32 +0300

    Add TrainErrors in Masar.Domain\Trains\TrainErrors.cs And Update Train in Masar.Domain\Trains\Train.cs

--- src/Masar.Domain/Trains/Train.cs
+++ src/Masar.Domain/Trains/Train.cs
@@ -1,35 +1,38 @@
 ﻿using Masar.Domain.Carriages;
 using Masar.Domain.Common;
+using Masar.Domain.Common.Results;
+using Masar.Domain.Persons;
 using Masar.Domain.Stations;
 using Masar.Domain.Trips;
+using Masar.Domain.Users;
 using System;
 using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace Masar.Domain.Trains;
 
 public partial class Train : AuditableEntity
 {
-    public string Code { get; set; } = null!;
+    public string Code { get; private set; } = null!;
 
-    public string Name { get; set; } = null!;
+    public string Name { get; private set; } = null!;
 
-    public string TrainType { get; set; } = null!;
+    public string TrainType { get; private set; } = null!;
 
-    public int MaxSpeedKmh { get; set; }
+    public int MaxSpeedKmh { get; private set; }
 
-    public TrainStatus Status { get; set; }
+    public TrainStatus Status { get; private set; }
 
-    public DateTime CreatedAt { get; set; }
+    public bool IsDelete { get; private set; }
 
-    public bool IsDelete { get; set; }
+    public Guid? CurrentStationId { get; private set; }
 
-    public Guid? CurrentStationId { get; set; }
+    public virtual ICollection<Carriage> Carriages { get; private set; } = new List<Carriage>();
 
-    public virtual ICollection<Carriage> Carriages { get; set; } = new List<Carriage>();
+    public virtual ICollection<Trip> Trips { get; private set; } = new List<Trip>();
 
-    public virtual ICollection<Trip> Trips { get; set; } = new List<Trip>();
-
-    public virtual Station? Station { get; set; } = null!;
+    public virtual Station? Station { get; private set; } = null!;
 
     private Train() { }
 
@@ -40,15 +43,79 @@ public partial class Train : AuditableEntity
     string name,
     string trainType,
     int maxSpeedKmh,
-    TrainStatus status,
-    DateTime createdAt)
+    Guid currentStationId)
         :base(id)
     {
         Code = code;
         Name = name;
         TrainType = trainType;
         MaxSpeedKmh = maxSpeedKmh;
-        Status = status;
-        CreatedAt = createdAt;
+        CurrentStationId = currentStationId;
+
+        Status = TrainStatus.Active;
+        IsDelete = false;
     }
+
+    public static Result<Train> Create(
+    Guid id,
+    string code,
+    string name,
+    string trainType,
+    int maxSpeedKmh,
+    Guid currentStationId)
+    {
+        if (string.IsNullOrWhiteSpace(code))
+        {
+            return TrainErrors.CodeRequired;
+        }
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return TrainErrors.NameRequired;
+        }
+      
+        if (string.IsNullOrWhiteSpace(trainType))
+        {
+            return TrainErrors.TrainTypeRequired;
+        }
+
+        if (maxSpeedKmh <= 0)
+        {
+            return TrainErrors.InvalidMaxSpeedKmh;
+        }
+
+        return new Train(id, code, name, trainType, maxSpeedKmh,currentStationId);
+    }
+
+
+    public Result<Updated> Update(string code,string name,string trainType,int maxSpeedKmh)
+    {
+        if (string.IsNullOrWhiteSpace(code))
+        {
+            return TrainErrors.CodeRequired;
+        }
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return TrainErrors.NameRequired;
+        }
+
+        if (string.IsNullOrWhiteSpace(trainType))
+        {
+            return TrainErrors.TrainTypeRequired;
+        }
+
+        if (maxSpeedKmh <= 0)
+        {
+            return TrainErrors.InvalidMaxSpeedKmh;
+        }
+
+        Code = code;
+        Name = name;
+        TrainType = trainType;
+        MaxSpeedKmh = maxSpeedKmh;
+        return Result.Updated;
+    }
+
+
 }
