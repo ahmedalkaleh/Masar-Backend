6255eea28d6f21f6527db962316395e8ed1f9a4a
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-08-27 01:01:48 +0300

    feat(Application): Add CreateCarriageCommandValidator And CreateCarriageCommandHandler And CarriageMapper

---
+++ src/Masar.Application/Features/Carriages/Mappers/CarriageMapper.cs
@@ -0,0 +1,31 @@
+﻿using Masar.Application.Features.Carriages.Dtos;
+using Masar.Domain.Carriages;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Masar.Application.Features.Carriages.Mappers
+{
+    public static class CarriageMapper
+    {
+        public static CarriageDto ToDto(this Carriage carriage)
+        {
+            return new CarriageDto
+            {
+                CarriageId = carriage.Id,
+                TrainId = carriage.TrainId,
+                CarriageNumber = carriage.CarriageNumber,
+                ClassType = carriage.ClassType,
+                TotalSeats = carriage.TotalSeats
+            };
+        }
+
+        public static List<CarriageDto> ToDto(this IEnumerable<Carriage> entities)
+        {
+            return entities.Select(x => x.ToDto()).ToList();
+        }
+
+
+    }
+}
