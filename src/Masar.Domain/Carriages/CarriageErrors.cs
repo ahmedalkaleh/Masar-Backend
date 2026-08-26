dbd52a3995c08d9ee34f31cb9749b2f90deeea94
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-08-27 00:56:54 +0300

    feat(Domain): Add CarriageErrors in Masar.Domain\Carriages\CarriageErrors.cs And Update Carriage in Masar.Domain\Carriages\Carriage.cs

---
+++ src/Masar.Domain/Carriages/CarriageErrors.cs
@@ -0,0 +1,47 @@
+﻿using Masar.Domain.Common.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Domain.Carriages
+{
+    public static class CarriageErrors
+    {
+
+        public static Error TrainIdRequired =>
+            Error.Validation(
+                "Carriage.TrainIdRequired",
+                "Train ID is required.");
+
+        public static Error InvalidCarriageNumber =>
+            Error.Validation(
+                "Carriage.InvalidCarriageNumber",
+                "Carriage number cannot be negative.");
+
+        public static Error ClassTypeRequired =>
+            Error.Validation(
+                "Carriage.ClassTypeRequired",
+                "Carriage class type is required.");
+
+        public static Error InvalidTotalSeats =>
+            Error.Validation(
+                "Carriage.InvalidTotalSeats",
+                "Total seats must be greater than 0.");
+
+        public static Error CarriageNotFound =>
+            Error.NotFound(
+                "Carriage.NotFound",
+                "Carriage with the specified ID was not found.");
+
+        public static Error TrainNotFound =>
+            Error.NotFound(
+                "Carriage.TrainNotFound",
+                "Train with the specified ID was not found.");
+
+        public static Error CarriageNumberAlreadyExists =>
+            Error.Conflict(
+                "Carriage.CarriageNumberAlreadyExists",
+                "A carriage with this number already exists.");
+
+    }
+}
