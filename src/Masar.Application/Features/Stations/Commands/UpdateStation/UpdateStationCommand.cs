5b116993728395cc3bb3f5160261c04e04633dbc
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-08-30 02:05:00 +0300

    feat(Application): Add UpdateStationCommand.cs And UpdateStationCommandHandler.cs And UpdateStationCommandValidator.cs

---
+++ src/Masar.Application/Features/Stations/Commands/UpdateStation/UpdateStationCommand.cs
@@ -0,0 +1,21 @@
+﻿using Masar.Domain.Common.Results;
+using Masar.Domain.Stations;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Stations.Commands.UpdateStation
+{
+    public sealed record UpdateStationCommand(
+    Guid StationID,
+    string NameAr,
+    string NameEn,
+    StationType Type,
+    decimal Latitude,
+    decimal Longitude,
+    string Governorate,
+    int CustomsDelayMinutes) : IRequest<Result<Updated>>
+    {
+    }
+}
