1ca83608f8288858286b5f46969c3862b851128e
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-08-30 02:03:39 +0300

    feat(Application): Add CreateStationCommand And StationDto

---
+++ src/Masar.Application/Features/Stations/Commands/CreateStation/CreateStationCommand.cs
@@ -0,0 +1,21 @@
+﻿using Masar.Application.Features.Stations.Dtos;
+using Masar.Domain.Common.Results;
+using Masar.Domain.Stations;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Stations.Commands.CreateStation
+{
+    public sealed record CreateStationCommand(
+    string NameAr,
+    string NameEn,
+    StationType Type,
+    decimal Latitude,
+    decimal Longitude,
+    string Governorate,
+    int CustomsDelayMinutes) : IRequest<Result<StationDto>>
+    { }
+    
+}
