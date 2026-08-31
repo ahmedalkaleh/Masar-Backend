2bd92f82c30e7820f436dc08eb35351214bfd96c
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-08-31 03:26:56 +0300

    feat(Application): Add CreateSeatCommandValidator And CreateSeatCommandHandler And SeatMapper

---
+++ src/Masar.Application/Features/Seats/Commands/CreateSeat/CreateSeatCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Seats.Commands.CreateSeat
+{
+    public sealed class CreateSeatCommandValidator : AbstractValidator<CreateSeatCommand>
+    {
+        public CreateSeatCommandValidator()
+        {
+            RuleFor(x => x.CarriageId)
+                .NotEmpty()
+                .WithMessage("Carriage ID is required.");
+
+            RuleFor(x => x.RowNumber)
+                .NotEmpty()
+                .WithMessage("Row number is required.")
+                .MaximumLength(2)
+                .WithMessage("Row number must not exceed 2 characters.");
+
+            RuleFor(x => x.SeatType)
+                .IsInEnum()
+                .WithMessage("Seat type is invalid.");
+
+            RuleFor(x => x.ColumnNumber)
+                .InclusiveBetween((byte)1, (byte)6)
+                .WithMessage("Column number must be between 1 and 6.");
+
+        }
+    }
+}
---
+++ src/Masar.Application/Features/Seats/Mappers/SeatMapper.cs
@@ -0,0 +1,30 @@
+﻿using Masar.Application.Features.Seats.Dtos;
+using Masar.Domain.Seats;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Seats.Mappers
+{
+    public static class SeatMapper
+    {
+        public static SeatDto ToDto(this Seat seat)
+        {
+            return new SeatDto
+            {
+                SeatID = seat.Id,
+                CarriageId = seat.CarriageId,
+                RowNumber = seat.RowNumber,
+                ColumnNumber = seat.ColumnNumber,
+                SeatNumber = seat.SeatNumber,
+                SeatType = seat.SeatType,
+                IsActive = seat.IsActive
+            };
+        }
+
+        public static List<SeatDto> ToDto(this IEnumerable<Seat> entities)
+        {
+            return entities.Select(x => x.ToDto()).ToList();
+        }
+    }
+}
