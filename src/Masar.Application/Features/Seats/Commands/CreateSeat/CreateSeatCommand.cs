8f65a5a7c5846aa9a22b2bc158e6cdfe58fb8182
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-08-31 03:25:19 +0300

    feat(Application): Add CreateSeatCommand.cs And SeatDto.cs

---
+++ src/Masar.Application/Features/Seats/Commands/CreateSeat/CreateSeatCommand.cs
@@ -0,0 +1,18 @@
+﻿using Masar.Application.Features.Seats.Dtos;
+using Masar.Domain.Common.Results;
+using Masar.Domain.Seats;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Seats.Commands.CreateSeat
+{
+    public sealed record CreateSeatCommand(
+    Guid CarriageId,
+    string RowNumber,
+    byte ColumnNumber,
+    SeatType SeatType) : IRequest<Result<SeatDto>>
+    {
+    }
+}
---
+++ src/Masar.Application/Features/Seats/Dtos/SeatDto.cs
@@ -0,0 +1,25 @@
+﻿using Masar.Domain.Seats;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Seats.Dtos
+{
+    public class SeatDto
+    {
+        public Guid SeatID { get; set; }
+
+        public Guid CarriageId { get; set; }
+
+        public string SeatNumber { get; set; } = string.Empty;
+
+        public string RowNumber { get; set; } = string.Empty;
+
+        public byte ColumnNumber { get; set; }
+
+        public SeatType SeatType { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
