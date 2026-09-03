558d07d3c9fac77ce3dceff416487f7b8bdf5c2f
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-09-04 00:38:54 +0300

    feat(Application): Add CreateRouteSegmentCommandValidator And CreateRouteSegmentCommandHandler And RouteSegmentMapper

---
+++ src/Masar.Application/Features/RouteSegments/Mappers/RouteSegmentMapper.cs
@@ -0,0 +1,30 @@
+﻿using Masar.Application.Features.RouteSegments.Dtos;
+using Masar.Domain.RouteSegments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.RouteSegments.Mappers
+{
+    public static class RouteSegmentMapper
+    {
+        public static RouteSegmentDto ToDto(this RouteSegment routeSegment)
+        {
+            return new RouteSegmentDto
+            {
+                RouteSegmentID = routeSegment.Id,
+                FromStationId = routeSegment.FromStationId,
+                ToStationId = routeSegment.ToStationId,
+                TrackType = routeSegment.TrackType,
+                DistanceKm = routeSegment.DistanceKm,
+                EstPassengerTimeMin = routeSegment.EstPassengerTimeMin,
+                CorridorName = routeSegment.CorridorName
+            };
+        }
+
+        public static List<RouteSegmentDto> ToDto(this IEnumerable<RouteSegment> entities)
+        {
+            return entities.Select(x => x.ToDto()).ToList();
+        }
+    }
+}
