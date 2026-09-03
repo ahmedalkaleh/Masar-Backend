f39f1feb9edc8b69c55cfadef4e86cda6e9918ae
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-09-04 00:38:07 +0300

    feat(Application): Add CreateRouteSegmentCommand.cs And RouteSegmentDto.cs

---
+++ src/Masar.Application/Features/RouteSegments/Dtos/RouteSegmentDto.cs
@@ -0,0 +1,24 @@
+﻿using Masar.Domain.RouteSegments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.RouteSegments.Dtos
+{
+    public class RouteSegmentDto
+    {
+        public Guid RouteSegmentID { get; set; }
+
+        public Guid FromStationId { get; set; }
+
+        public Guid ToStationId { get; set; }
+
+        public TrackType TrackType { get; set; }
+
+        public decimal DistanceKm { get; set; }
+
+        public int EstPassengerTimeMin { get; set; }
+
+        public string CorridorName { get; set; } = String.Empty;
+    }
+}
