4eb8aa5e55fae5548a4b0428b4375bc4e2ffa26e
Ahmed <ahmodikaleeh2005@gmail.com>
2026-09-06 23:57:25 +0300

    feat(Domain) add RoutTemplate and RoutTemplate errors

---
+++ src/Masar.Domain/RouteTemplates/RoutTemplateErrors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Masar.Domain.Common.Results;
+namespace Masar.Domain.RouteTemplates
+{
+    public static class RoutTemplateErrors
+    {
+        public static Error TemplateNameRequired=> Error.Validation("RouteTemplate.TemplateNameRequired", "Template name is required.");
+
+        public static Error StartStationRequired => Error.Validation("RouteTemplate.StartStationRequired", "Start station is required.");
+
+        public static Error EndStationRequired => Error.Validation("RouteTemplate.EndStationRequired", "End station is required.");
+
+        public static Error StartStationNotFound => Error.NotFound("RouteTemplate.StartStationNotFound", "Start station with the specified ID was not found.");
+
+        public static Error EndStationNotFound => Error.NotFound("RouteTemplate.EndStationNotFound", "End station with the specified ID was not found.");
+
+        public static Error EndStationEqualStartStation => Error.Conflict("RouteTemplate.EndStationEqualStartStation","End Station must be different from Start Station");
+
+    }
+}
---
+++ src/Masar.Domain/RouteTemplates/RouteTemplate.cs
@@ -0,0 +1,96 @@
+﻿using Masar.Domain.Common.Results;
+using Masar.Domain.Common;
+
+using Masar.Domain.Stations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Domain.RouteTemplates
+{
+    public class RouteTemplate:AuditableEntity
+    {
+        public string TemplateName { get; private set; } = null!;
+        public Guid StartStationId { get; private set; }
+        public Guid EndStationId { get; private set; }
+
+        public Station StartStation { get; private set; } = null!;
+
+        public Station EndStation { get; private set; } = null!;
+        
+        public RouteTemplate() { }
+
+        private RouteTemplate(
+            Guid id,
+            string templateName,
+            Guid startStationId,
+            Guid endStationId
+            )
+            : base(id)
+        {
+            TemplateName = templateName;
+            StartStationId = startStationId;
+            EndStationId = endStationId;
+        }
+
+        public static Result<RouteTemplate> Create(
+            string templateName,
+            Guid startStationId,
+            Guid endStationId
+            )
+        {
+            if (string.IsNullOrEmpty(templateName))
+            {
+                return RoutTemplateErrors.TemplateNameRequired;
+            }
+            if(startStationId == Guid.Empty)
+            {
+                return RoutTemplateErrors.StartStationRequired;
+            }
+            if(endStationId == Guid.Empty)
+            {
+                return RoutTemplateErrors.EndStationRequired;
+            }
+            if(startStationId == endStationId)
+            {
+                return RoutTemplateErrors.EndStationEqualStartStation;
+            }
+
+            return new RouteTemplate(
+                Guid.NewGuid(),
+                templateName,
+                startStationId,
+                endStationId
+                );
+        }
+
+        public Result<Updated> Update(
+            
+            string templateName,
+            Guid startStationId,
+            Guid endStationId
+            )
+        {
+            if (string.IsNullOrEmpty(templateName))
+            {
+                return RoutTemplateErrors.TemplateNameRequired;
+            }
+            if (startStationId == Guid.Empty)
+            {
+                return RoutTemplateErrors.StartStationRequired;
+            }
+            if (endStationId == Guid.Empty)
+            {
+                return RoutTemplateErrors.EndStationRequired;
+            }
+            if (startStationId == endStationId)
+            {
+                return RoutTemplateErrors.EndStationEqualStartStation;
+            }
+            TemplateName = templateName;
+            StartStationId = startStationId;
+            EndStationId = endStationId;
+            return Result.Updated;
+        }
+    }
+}
