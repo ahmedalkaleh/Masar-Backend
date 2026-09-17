02151c68e2326b69b36b6e85f4b92cf6de66b21c
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-09-17 22:20:50 +0300

    feat(Application): Add CreateBookingCommand.cs And BookingDto.cs

---
+++ src/Masar.Application/Features/Bookings/Dtos/BookingDto.cs
@@ -0,0 +1,30 @@
+﻿using Masar.Domain.Bookings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Bookings.Dtos
+{
+    public class BookingDto
+    {
+        public Guid BookingID { get; set; }
+
+        public string BookingReference { get; set; } = string.Empty;
+
+        public Guid PassengerId { get;  set; }
+
+        public Guid TripId { get; set; }
+
+        public Guid BoardingStationId { get; set; }
+
+        public Guid AlightingStationId { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public PaymentStatus PaymentStatus { get; set; }
+
+        public DateTime? PaidAt { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+    }
+}
