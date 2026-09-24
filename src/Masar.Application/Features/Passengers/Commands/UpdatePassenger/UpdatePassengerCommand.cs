f22e52d6f857011ae0c7d39e3b2c7c621cdb1ce4
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-09-24 20:17:04 +0300

    feat(Application): Add UpdatePassengerCommandValidator And UpdatePassengerCommandValidator And UpdatePassengerCommandHandler

---
+++ src/Masar.Application/Features/Passengers/Commands/UpdatePassenger/UpdatePassengerCommand.cs
@@ -0,0 +1,18 @@
+﻿using Masar.Application.Features.Passengers.Dtos;
+using Masar.Application.Features.Persons.Commands.CreatePerson;
+using Masar.Application.Features.Persons.Commands.UpdatePerson;
+using Masar.Domain.Common.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Passengers.Commands.UpdatePassenger
+{
+    public sealed record UpdatePassengerCommand(
+        Guid PassengerID,
+        CreatePersonCommand Person
+        ) : IRequest<Result<Updated>>
+    {
+    }
+}
