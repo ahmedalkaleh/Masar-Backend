71cbdaf3ab2edfbb2283ad798dffd4aa006f095b
Mohammed Salem <mhamadsalem0936@gmail.com>
2026-09-24 20:09:04 +0300

    feat(Application): Add CreatePassengerCommand.cs And PassengerDto.cs

---
+++ src/Masar.Application/Features/Passengers/Dtos/PassengerDto.cs
@@ -0,0 +1,14 @@
+﻿using Masar.Application.Features.Persons.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masar.Application.Features.Passengers.Dtos
+{
+    public class PassengerDto
+    {
+        public Guid PassengerID {  get; set; }
+
+        public PersonDto person { get; set; } = null!;
+    }
+}
